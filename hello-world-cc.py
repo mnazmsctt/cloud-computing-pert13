3423bab023616c5f39376eeec0101e8e603a2da0
mnazmsctt <mohammadnazmifirdaus@gmail.com>
2026-05-19 15:12:33 +0700

    firstcommit

---
+++ hello-world-cc.py
@@ -0,0 +1,9 @@
+# Program Hello World - Cloud Computing
+# Nama  : Mohamad Nazmi Firdaus
+# NIM   : 241011402802
+# Kelas : 04TPLP027
+
+print("Hello, World!")
+print("Selamat datang di Cloud Computing!")
+print("Nama: Mohamad Nazmi Firdaus")
+print("NIM : 241011402802")
